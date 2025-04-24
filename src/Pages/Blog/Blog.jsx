35192ef5e825865b05fce292ed7ae80f33c4c2f2
riy8n <mahafujhossainriyan02@gmail.com>
2025-04-24 18:56:48 +0600

    added the final change

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -5,8 +5,8 @@ const Blog = ({blog}) => {
     const { question, answer } = blog || {};
 
     return (
-        <div className='border border-[rgba(15,15,15,0.1)] bg-[rgba(15,15,15,0.05)] p-2'>
-            <h1 className='font-bold'>{question}</h1>
+        <div className='border border-[rgba(15,15,15,0.1)] bg-[rgba(15,15,15,0.05)] p-2 rounded-xl'>
+            <h1 className='font-bold'> {question}</h1>
             <p>{answer}</p>
         </div>
     );
